bce29bf386675eed41077952e6aa8dd7c369b106
PaoloNapoli97 <paolonapoli@live.it>
2023-01-26 19:02:13 +0100

    first commit

---
+++ resources/views/admin/projects/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Lista Progetti</h1>
+    <table class="table table-striped table-dark">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Azioni</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <td>{{ $project->id }}</td>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td></td>
+                </tr>
+            @endforeach
+
+        </tbody>
+      </table>
+@endsection
